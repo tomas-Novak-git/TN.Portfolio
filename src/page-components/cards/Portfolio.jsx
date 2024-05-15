4dc2583ea34591c55bdfc059c177d554050f1f82
tomas-novak-git <tomas_novak@centrum.cz>
2024-05-15 11:25:39 +0200

    readme edit

--- src/page-components/cards/Portfolio.jsx
+++ src/page-components/cards/Portfolio.jsx
@@ -16,8 +16,8 @@ const Portfolio = () => {
             Tento menší portofolio web vznikl pro ukázku mé dosavadní práce.
             Samozřejmě bude stránka aktualizovaná s budoucími projekty. Nesdílím
             tu komerční zakázky, spíše své osobní práce. Vybral jsem si
-            technologii React, Tailwind a čistý Javascript. Do budoucna bych rád
-            získal zkušenosti s dalšími CSS preprocessory a frameworky.
+            technologii React, Vite, Tailwind a čistý Javascript. Do budoucna
+            bych rád získal zkušenosti s dalšími CSS preprocessory a frameworky.
           </p>
         </div>
         <img
